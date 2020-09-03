1709723462982a58a44b1cb236478584c90cb723
yelgon <yelgon36@gmail.com>
2020-09-03 18:15:40 -0400

    social link test

---
+++ public/build/precache-manifest.17f7b698be3da885dc0a41f02bf653f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cd9fde55dde66851efff491686e417e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e02b9359527b3f65a55",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "1c17ed81fafe3e99ccbc",
+    "url": "/static/js/2.18099e66.chunk.js"
+  },
+  {
+    "revision": "cdf6e6b34674f199330e65ec0e02d3ee",
+    "url": "/static/js/2.18099e66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e02b9359527b3f65a55",
+    "url": "/static/js/main.036e7cce.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
