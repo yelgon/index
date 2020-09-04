0b3bf4e0924b050e7fa88c38db6011e0cdca58c8
YK <yelgon36@gmail.com>
2020-09-04 12:51:06 -0400

    heroku finish

---
+++ public/precache-manifest.e38294e6a60fd15f912d32518456bcf6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fd4bb9e3caf61c29d212ef85c2d6291",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2358bf52091c8fd0eea5",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "48712af293a6389e67ad",
+    "url": "/static/js/2.86ffbcb6.chunk.js"
+  },
+  {
+    "revision": "cdf6e6b34674f199330e65ec0e02d3ee",
+    "url": "/static/js/2.86ffbcb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2358bf52091c8fd0eea5",
+    "url": "/static/js/main.23003e3b.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
